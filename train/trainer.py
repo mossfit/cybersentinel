d3b11de480b03b3e20c73f0c189ed7a752f7407d
Mainak <108523198+mossfit@users.noreply.github.com>
2025-04-09 21:14:21 +0900

    Update trainer.py

--- train/trainer.py
+++ train/trainer.py
@@ -1,12 +1,33 @@
-def train(model, loader, optimizer, criterion, epochs=50):
-    model.train()
-    for epoch in range(epochs):
-        for img_high, img_low, labels in loader:
-            preds = model(img_high, img_low)
-            loss = criterion(preds, labels)
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from utils.config import config
 
+def train_model(model, train_loader, val_loader, num_epochs=config['num_epochs']):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=config['learning_rate'], weight_decay=1e-4)
+    
+    device = config['device']
+    model.to(device)
+    
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        for images, labels in train_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            outputs = model(images, images)
+            loss = criterion(outputs, labels)
+            
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-
-        print(f"Epoch {epoch}: Loss {loss.item():.4f}")
+            
+            running_loss += loss.item() * images.size(0)
+        epoch_loss = running_loss / len(train_loader.dataset)
+        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}")
+        
+     
+        
+    return model
