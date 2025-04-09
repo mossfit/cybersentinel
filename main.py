fedda4fe0c40a7ac41b4a57cf8da71e12fc5bc8c
Mainak <108523198+mossfit@users.noreply.github.com>
2025-04-09 21:18:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from preprocessing.dataset_build import build_dataset
-from training.train import train_model
-from evaluation.evaluate import evaluate_model
+import torch
+from utils.data_loader import get_data_loaders
+from networks.cybersentinel_model import CyberSentinel
+from training.trainer import train_model
+from utils.config import config
 
 def main():
-    build_dataset('./dataset/raw_pe_files/', './dataset/images/')
-    train_model()
-    evaluate_model()
-
+    train_dir = './dataset/images/train'
+    val_dir = './dataset/images/val'
+    
+    train_loader, val_loader = get_data_loaders(train_dir, val_dir, 
+                                                batch_size=config['batch_size'], 
+                                                image_size=config['train_image_size'])
+    
+    # Instantiate the model
+    model = CyberSentinel(num_classes=config['num_classes'])
+    
+    # Train the model
+    model = train_model(model, train_loader, val_loader, num_epochs=config['num_epochs'])
+    
+   
+    
 if __name__ == "__main__":
     main()
